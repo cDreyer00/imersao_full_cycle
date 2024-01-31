46fc2a999dfd2e728903dba22841a1e557d36276
cdreyer <cristianbr.dreyer@gmail.com>
2024-01-31 18:07:52 -0300

    goapi initial commit

---
+++ cmd/catalog/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/cDreyer00/devfullcycle/gopai/internal/database"
+	"github.com/cDreyer00/devfullcycle/gopai/internal/service"
+	"github.com/cDreyer00/devfullcycle/gopai/internal/webserver"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/devfullcycle")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategoryDB(db)
+	productDB := database.NewProductDB(db)
+
+	categoryService := service.NewCategoryService(*categoryDB)
+	productService := service.NewProductService(*productDB)
+
+	webCategoryHandler := webserver.NewWebCategoryHandler(categoryService)
+	webProductHandler := webserver.NewWebProductHandler(productService)
+
+	c := chi.NewRouter()
+	c.Use(middleware.Logger)
+	c.Use(middleware.Recoverer)
+	c.Get("/categories", webCategoryHandler.GetCategories)
+	c.Get("/category/{id}", webCategoryHandler.GetCategory)
+	c.Post("/category", webCategoryHandler.CreateCategory)
+
+	c.Get("/products", webProductHandler.GetProducts)
+	c.Get("/product/{id}", webProductHandler.GetProduct)
+	c.Get("/product/category/{id}", webProductHandler.GetProductsByCategoryID)
+	c.Post("/product", webProductHandler.CreateProduct)
+
+	fmt.Println("Server running on http://localhost:3000")
+	http.ListenAndServe(":3000", c)
+}
---
+++ internal/entity/product_entity.go
@@ -0,0 +1,22 @@
+package entity
+
+import "github.com/google/uuid"
+
+type Product struct {
+	ID          string
+	Name        string
+	Description string
+	Price       float64
+	CategoryID  string
+	ImageURL    string
+}
+
+func NewProduct(name string, description string, price float64, categoryID string, imageUrl string) *Product {
+	return &Product{
+		ID:          uuid.New().String(),
+		Name:        name,
+		Description: description,
+		Price:       price,
+		CategoryID:  categoryID,
+	}
+}
